3b7e98ac63b26228a75419a2fcecd67c29b7a732
Zachary Newman <zjn@mit.edu>
2019-05-28 12:21:51 -0400

    Add dummy function and verify that test suite works

---
+++ iacr/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+def double(x: int) -> int:
+    return x * 2
+
+
+def main() -> int:
+    pass
+
+
+if __name__ == "__main__":
+    main()
--- iacr/iacr_test.py
+++ iacr/iacr_test.py
@@ -2,10 +2,12 @@
 
 import unittest
 
+from . import double
+
 
 class TestSuite(unittest.TestCase):
-    def test_true(self):
-        self.assertTrue(True)
+    def test_foo(self) -> None:
+        self.assertEqual(double(2), 4)
 
 
 if __name__ == "__main__":
