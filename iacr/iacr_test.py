5395e8950d2c75b2733875c853e256ece5e67514
Zachary Newman <zjn@mit.edu>
2019-05-29 09:46:03 -0400

    Rename _get_html -> _get_test_resource
    There's nothing HTML-specific about it, and we're going to use it for other
    files.

--- iacr/iacr_test.py
+++ iacr/iacr_test.py
@@ -13,7 +13,7 @@ from . import Article
 TEST_DATA_DIR = Path(__file__).parent.absolute() / "data"
 
 
-def _get_html(fname):
+def _get_test_resource(fname):
     with open(TEST_DATA_DIR / fname) as f:
         return f.read()
 
@@ -21,7 +21,7 @@ def _get_html(fname):
 class ArticleTests(unittest.TestCase):
     # modified from https://ia.cr/2019/579
     def test_parse_html_basic(self) -> None:
-        actual = Article.parse_html(_get_html("basic.html"))
+        actual = Article.parse_html(_get_test_resource("basic.html"))
         expected = Article(
             "BlockQuick: Super-Light Client Protocol for Blockchain",
             ["Dominic Letz"],
@@ -36,7 +36,7 @@ class ArticleTests(unittest.TestCase):
 
     # modified from https://ia.cr/2019/549
     def test_parse_html_multi_author(self):
-        article = Article.parse_html(_get_html("multi-paragraph.html"))
+        article = Article.parse_html(_get_test_resource("multi-paragraph.html"))
         self.assertEqual(
             article.authors,
             [
